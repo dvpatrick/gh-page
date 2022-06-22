428f038d92ed2529876a39b0f287ca1bc9f27f1f
dvpatrick <patrickafb2020@gmail.com>
2022-06-22 18:57:18 -0300

    init

---
+++ src/Components/Body.js
@@ -0,0 +1,24 @@
+import './Body.css';
+import Projects from "./Projects";
+
+
+function body () {
+  return (
+    <div className="body">
+      <div className="apresentacao">
+        <p>
+          Hi,
+          <br />
+          I'm Patrick,
+          <br />
+          web developer
+        </p>
+        <button className="btn">
+          Contact me!
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default body;
