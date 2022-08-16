be90cd3301f9b6fc8f6a1664beed7e5347f4cdcd
dvpatrick <patrickafb2020@gmail.com>
2022-08-16 11:34:39 -0300

    feat: impplementacoes no form para receber feedbacks

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -12,10 +12,10 @@ function contact() {
           alguma duvida ou feedback, seja positivo ou negativo, nao hesite em me mandar <br />
           por esse formulario. <br /> <br />
         </p>
-        <form id="formulario" name="formularioLogin" method="post" netlify data-netlify="true">
-        <input type="hidden" name="form-name" value="formularioLogin" />
-        {/* <form id="formulario" name="formularioLogin" action="/"> */}
-        {/* <form id="formulario" name="formularioLogin" data-netlify="true"> */}
+        <form name="formularioLogin" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="formularioLogin" />
+          {/* <form id="formulario" name="formularioLogin" action="/"> */}
+          {/* <form id="formulario" name="formularioLogin" data-netlify="true"> */}
           <fieldset>
             <input type="text" name="nome" placeholder='Nome'></input>
             <input type="email" name="email" placeholder='Email'></input> <br />
